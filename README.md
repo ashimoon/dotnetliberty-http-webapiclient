af9ba61bbe79351679f15fdfa0b0c6f3e1c77203
Armen Shimoon <armen.shimoon@gmail.com>
2015-12-05 22:57:19 -0800

    Changing package name.

--- README.md
+++ README.md
@@ -1,1 +1,96 @@
-﻿# Test
+﻿# Description
+
+A simple client for interacting with a RESTful APIs using strongly typed models on .NET Core.
+
+Supports synchronous and asynchronous `GET`/`POST`/`PUT`/`DELETE`.
+
+# Usage
+
+## Setup
+Declare a model:
+```
+public class Thing
+{
+    public int Id { get; set; }
+    public string SomeProperty { get; set; }
+}
+```
+
+Create client:
+```
+// Second type parameter is the ID type
+var client = new WebApiClient<Thing, int>(
+    new Uri("http://localhost:5000/api/things/"));
+```
+
+## GET /api/things/
+
+```
+// Async
+IEnumerable<Thing> things = await _client.GetAsync();
+// Sync
+IEnumerable<Thing> things = _client.Get();
+```
+
+## GET /api/things/{id}
+
+```
+var id = 1;
+// Async
+Thing thing = await _client.GetAsync(id);
+// Sync
+Thing thing = _client.Get(id);
+```
+
+## POST /api/things/
+
+```
+// Async
+Thing created = await _client.PostAsync(new Thing
+{
+    Id = 2,
+    SomeProperty = "Some value"
+});
+
+// Sync
+Thing created = _client.Post(new Thing
+{
+    Id = 2,
+    SomeProperty = "Some value"
+});
+```
+
+## PUT /api/things/{id}
+
+```
+var id = 3;
+// Async
+Thing updated = await _client.PutAsync(id, new Thing
+{
+    Id = id,
+    SomeProperty = "Some value"
+});
+
+// Sync
+Thing updated = _client.Put(id, new Thing
+{
+    Id = id,
+    SomeProperty = "Some value"
+});
+```
+
+## DELETE /api/things/{id}
+
+```
+var id = 4;
+// Async
+await _client.DeleteAsync(id);
+
+// Sync
+_client.Delete(id);
+```
+
+
+# More Samples
+
+Take a look at the `Samples/` folder.
---
+++ Samples/00Basics.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetLiberty.Http.Samples
+{
+    public class Basics
+    {
+        private readonly WebApiClient<Thing, int> _client;
+
+        public class Thing
+        {
+            public int Id { get; set; }
+            public string SomeProperty { get; set; }
+        }
+
+        public Basics()
+        {
+            // Second type parameter is the ID type
+            _client = new WebApiClient<Thing, int>(
+                new Uri("http://localhost:5000/api/things/"));
+        }
+
+        public async void GetManyAsync()
+        {
+            // GET /api/things/
+            IEnumerable<Thing> things = await _client.GetAsync();
+        }
+
+        public void GetMany()
+        {
+            // GET /api/things/
+            IEnumerable<Thing> things = _client.Get();
+        }
+
+        public async void GetSingleAsync()
+        {
+            // GET /api/things/1
+            Thing thing = await _client.GetAsync(1);
+        }
+
+        public void GetSingle()
+        {
+            // GET /api/things/1
+            Thing thing = _client.Get(1);
+        }
+
+        public async void PostAsync()
+        {
+            Thing created = await _client.PostAsync(new Thing
+            {
+                Id = 2,
+                SomeProperty = "Some value"
+            });
+        }
+
+        public void Post()
+        {
+            Thing created = _client.Post(new Thing
+            {
+                Id = 2,
+                SomeProperty = "Some value"
+            });
+        }
+
+        public async void PutAsync()
+        {
+            Thing updated = await _client.PutAsync(3, new Thing
+            {
+                Id = 3,
+                SomeProperty = "Some value"
+            });
+        }
+
+        public void Put()
+        {
+            Thing updated = _client.Put(3, new Thing
+            {
+                Id = 3,
+                SomeProperty = "Some value"
+            });
+        }
+
+        public async void DeleteAsync()
+        {
+            await _client.DeleteAsync(4);
+        }
+
+        public void Delete()
+        {
+            _client.Delete(4);
+        }
+    }
+}
